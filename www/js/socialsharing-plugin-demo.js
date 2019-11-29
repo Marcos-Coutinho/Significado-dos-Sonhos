68e881ad8cabc82d18f77923f2b082de2986bfaf
Marcos-Coutinho <43754947+Marcos-Coutinho@users.noreply.github.com>
2019-11-29 00:19:34 -0400

    Delete socialsharing-plugin-demo.js

--- www/js/socialsharing-plugin-demo.js
+++
@@ -1,8 +0,0 @@
-function socialsharingDemo(titulo, imagem) {
-  $('#btnCompartilhar').on("tap click", function () {
-     window.plugins.socialsharing.share(titulo, null, imagem, null);
-  });
-}
-
-
-SocialShare(tituloParaCompartilhar, urlDaSuaImagem); //Chamando meu método
